2080fc45099bcdce360b308334b8f65035f95d28
Jannick Hemelhof <mister.jannick@gmail.com>
2017-07-23 11:38:38 +0200

    Preparations for route generation

--- railway/route-element.rkt
+++ railway/route-element.rkt
@@ -9,11 +9,12 @@
         [type-index 0]
         [id-index 1]
         [length-index 2]
-        [switch-index 3])
+        [done-distance-index 3]
+        [switch-index 4])
 
     (if (eq? mode -1)
-        (set! route-element (vector type id length))
-        (set! route-element (vector type id length mode)))
+        (set! route-element (vector type id length 0))
+        (set! route-element (vector type id length 0 mode)))
 
     ; Function that will return the type of the route element
     ; @return -> type of route element
@@ -35,10 +36,20 @@
     (define (set-length! new-length)
       (vector-set! route-element length-index new-length))
 
+    ; Function that will return the done distance of the route element
+    ; @return -> done distance of route element (number)
+    (define (get-done-distance)
+      (vector-ref route-element done-distance-index))
+
+    ; Function that will modify the done distance of the route element
+    ; @param new-done-distance -> new done distance of the route element
+    (define (set-done-distance! new-done-distance)
+      (vector-set! route-element done-distance-index new-done-distance))
+
     ; Function that will return the mode of the switch (optional fourth argument)
     ; @return -> switch mode
     (define (get-switch-mode)
-      (if (eq? (vector-length route-element) 3)
+      (if (eq? (vector-length route-element) 4)
           (error "Not a switch, cannot return switch mode")
           (vector-ref route-element switch-index)))
 
@@ -48,6 +59,8 @@
         ((get-id) get-id)
         ((get-length) get-length)
         ((set-length!) set-length!)
+        ((get-done-distance) get-done-distance)
+        ((set-done-distance!) set-done-distance!)
         ((get-switch-mode) get-switch-mode)))
 
     dispatch))
